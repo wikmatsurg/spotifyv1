6a70c2c896ee8a96d0c2ec1ae11fc0b848c68bc0
wik <81856716+wikmatsurg@users.noreply.github.com>
2026-08-29 17:59:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -101,7 +101,6 @@ async function getTopSongs(token) {
     const result = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=${songNum}", {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` }
-        })
     });
     return await result.json();
 }
